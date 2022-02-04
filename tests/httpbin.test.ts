e455cb0158e3a288b40020d7ab1f715ba1a6f82f
Andrew Plummer <plummer574@gmail.com>
2022-02-04 21:06:54 +0000

    Integration test with httpbin and fix (#4)

---
+++ tests/httpbin.test.ts
@@ -0,0 +1,35 @@
+import { fetchExecutable, Options } from '..';
+import * as tmp from 'tmp';
+import * as pathlib from 'path';
+import * as fs from 'fs';
+import { execSync } from 'child_process';
+
+tmp.setGracefulCleanup();
+
+const sampleExecutableFileContent = ['#!/bin/bash', '', 'echo 1.2.3', ''].join('\n');
+
+const base64encode = (str: string): string => {
+    return Buffer.from(str).toString('base64');
+};
+
+describe('httpbin', () => {
+    test('get from httpbin', async () => {
+        const dir = tmp.dirSync();
+
+        const options = new Options({
+            url: `https://httpbin.org/base64/${base64encode(sampleExecutableFileContent)}`,
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            execIsOk: async (filepath: string): Promise<boolean> => false,
+        });
+
+        await fetchExecutable({
+            target: pathlib.join(dir.name, 'testexc'),
+            options,
+        });
+
+        expect(fs.readFileSync(pathlib.join(dir.name, 'testexc'), 'utf8')).toEqual(sampleExecutableFileContent);
+        expect(execSync(pathlib.join(dir.name, 'testexc'), { encoding: 'utf8' }).trim()).toEqual('1.2.3');
+
+        fs.rmdirSync(dir.name, { recursive: true });
+    });
+});

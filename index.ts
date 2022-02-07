3e70ebfdd2b73ccbf93e277f5e01be52c9e8587a
Andrew Plummer <plummer574@gmail.com>
2022-02-07 22:39:34 +0000

    Support tar and gzip (#9)

--- index.ts
+++ index.ts
@@ -1,29 +1,10 @@
-import axios from 'axios';
-import format from 'string-format';
-import { execSync } from 'child_process';
 import * as fs from 'fs';
-
-const readFromStream = (stream: NodeJS.ReadableStream, dest: string): Promise<void> => {
-    return new Promise((res, rej) => {
-        try {
-            fs.unlinkSync(dest);
-        } catch (err) {
-            if (err.code === 'ENOENT') {
-                // That's fine
-            } else {
-                throw err;
-            }
-        }
-        const output = fs.createWriteStream(dest);
-        output.on('finish', () => {
-            res();
-        });
-        output.on('error', (err) => {
-            rej(err);
-        });
-        stream.pipe(output);
-    });
-};
+import { execSync } from 'child_process';
+import * as streamlib from 'stream';
+import * as zlib from 'zlib';
+import * as tar from 'tar-stream';
+import format from 'string-format';
+import axios from 'axios';
 
 type ExecIsOk = (filepath: string) => Promise<boolean>;
 
@@ -33,6 +14,8 @@ export interface Options {
     version?: string;
     versionExecArgs?: Array<string>;
     versionExecPostProcess?: (execOutput: string) => string;
+    pathInTar?: string;
+    gzExtract?: boolean;
 }
 
 const optsExecIsOk = (opts: Options): ExecIsOk => {
@@ -66,6 +49,73 @@ const optsUrl = (opts: Options): string => {
     });
 };
 
+const saveFromStream = (stream: NodeJS.ReadableStream, dest: string): Promise<void> => {
+    return new Promise((res, rej) => {
+        try {
+            fs.unlinkSync(dest);
+        } catch (err) {
+            if (err.code === 'ENOENT') {
+                // That's fine
+            } else {
+                throw err;
+            }
+        }
+
+        const output = fs.createWriteStream(dest);
+        output.on('finish', () => {
+            res();
+        });
+        output.on('error', (err) => {
+            rej(err);
+        });
+        stream.pipe(output);
+    });
+};
+
+const extractFromTar = (inStream: NodeJS.ReadableStream, pathInTar: string): NodeJS.ReadableStream => {
+    const extract = tar.extract();
+
+    const outStream = new streamlib.PassThrough();
+    let found = false;
+
+    extract.on('entry', (header, entryStream, next) => {
+        if (header.name === pathInTar) {
+            found = true;
+            entryStream.pipe(outStream);
+        }
+
+        entryStream.on('end', () => {
+            next();
+        });
+
+        entryStream.resume();
+    });
+
+    extract.on('finish', () => {
+        if (!found) {
+            throw new Error(`Failed to find ${pathInTar} in tar`);
+        }
+    });
+
+    inStream.pipe(extract);
+
+    return outStream;
+};
+
+const optsSave = async (opts: Options, stream: NodeJS.ReadableStream, dest: string): Promise<void> => {
+    let processedStream = stream;
+
+    if (opts.gzExtract) {
+        processedStream = processedStream.pipe(zlib.createGunzip());
+    }
+
+    if (opts.pathInTar) {
+        processedStream = extractFromTar(processedStream, opts.pathInTar);
+    }
+
+    await saveFromStream(processedStream, dest);
+};
+
 export interface FetchExecutableProps {
     target: string;
     options: Options;
@@ -86,6 +136,6 @@ export const fetchExecutable = async (props: FetchExecutableProps): Promise<void
         responseType: 'stream',
     });
 
-    await readFromStream(response.data, props.target);
+    await optsSave(props.options, response.data, props.target);
     fs.chmodSync(props.target, 0o755);
 };

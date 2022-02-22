1dcaf20eccc9e582031a7147f2c132a0ddb702b9
Andrew Plummer <plummer574@gmail.com>
2022-02-22 20:32:10 +0000

    Remove custom types for unbzip2-stream (#25)

--- types/unbzip2-stream.d.ts
+++
@@ -1,9 +0,0 @@
-// This can be replaced once https://www.npmjs.com/package/@types/unbzip2-stream exists.
-// See https://github.com/DefinitelyTyped/DefinitelyTyped/pull/58828
-declare module 'unbzip2-stream' {
-    import through = require('through');
-
-    declare function bz2(): through.ThroughStream;
-
-    export = bz2;
-}
